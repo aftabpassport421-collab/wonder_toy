386d35c12571648234802f284697e3a971aec6c8
aftabpassport421-collab <aftabpassport421@gmail.com>
2026-09-22 19:15:59 +0300

    Update build.gradle.kts for plugin configurations
    Removed several plugin declarations and added Kotlin Android plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-    id("io.github.takahirom.roborazzi") version "1.12.0" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
