86ef8ef9b550817c8fd73ee73336fe8e91c8b47c
aftabpassport421-collab <aftabpassport421@gmail.com>
2026-09-22 19:15:45 +0300

    Refactor build.gradle.kts for Android app setup
    Updated Gradle configuration to include Android and Kotlin plugins, and set application details.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,23 @@
-
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-    id("io.github.takahirom.roborazzi") version "1.12.0" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.wonder_toy"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.wonder_toy"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
 }
